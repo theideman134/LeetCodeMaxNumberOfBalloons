5b55824186417522071be47958ab170219801eb7
Todd Heideman <61336585+theideman134@users.noreply.github.com>
2020-12-08 16:37:14 -0600

    first attempt of ballon numberB

--- LeetCodeMaxNumberOfBalloons/Solution.cs
+++ LeetCodeMaxNumberOfBalloons/Solution.cs
@@ -11,7 +11,66 @@ namespace LeetCodeMaxNumberOfBalloons
         */
         public int MaxNumberOfBalloons(string text)
         {
-            return 0;
+            if (String.IsNullOrEmpty(text))
+                return 0;
+            
+            int numB = 0;
+            int numA = 0;
+            int numL = 0;
+            int numN = 0;
+            int numO = 0;
+
+            foreach (char letter in text.ToCharArray())
+            {
+
+                switch (letter)
+                {
+                    case 'b':
+                        numB++;
+                        break;
+                    case 'a':
+                        numA++;
+                        break;
+                    case 'l':
+                        numL++;
+                        break;
+                    case 'o':
+                        numO++;
+                        break;
+                    case 'n':
+                        numN++;
+                        break;
+
+                }
+            }
+
+            int lowestNum = LowestNumber(numB, numA,false);
+            lowestNum = LowestNumber(lowestNum, numL,true);
+            lowestNum = LowestNumber(lowestNum, numO, true);
+            lowestNum = LowestNumber(lowestNum, numN, false);
+
+            return lowestNum;
+
         }
+
+        public int LowestNumber(int lowestNumber, int currentNum,bool doubleLetter)
+        {
+            if (doubleLetter)
+            {
+                if (lowestNumber * 2 > currentNum)
+                    return currentNum / 2;
+
+            }
+            else
+            {
+                if (lowestNumber > currentNum)
+                    return currentNum;
+            }
+
+            return lowestNumber;
+        }
+
+
+
     }
 }
--- MaxNumBalloonsTest/SolutionTest.cs
+++ MaxNumBalloonsTest/SolutionTest.cs
@@ -37,5 +37,54 @@ namespace MaxNumBalloonsTest
             var result = solution.MaxNumberOfBalloons(balloonString);
             Assert.AreEqual(0, result);
         }
+
+        [Test]
+        public void Test4()
+        {
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(string.Empty);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+ 
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(null);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void Test6()
+        {
+            string balloonString = "bballoon";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(1, result);
+        }
+        [Test]
+        public void Test7()
+        {
+            string balloonString = "bbaallllloooonn";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(2, result);
+        }
+        [Test]
+        public void Test8()
+        {
+            string balloonString = "bbbaaallllloooooonnn";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(2, result);
+        }
+        [Test]
+        public void Test9()
+        {
+            string balloonString = "bbbaaallllllloooooonnn";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(3, result);
+        }
     }
 }
