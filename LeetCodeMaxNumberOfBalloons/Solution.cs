ffaa6fdf0c8f219a620208dfdc292d6286a6df4d
Todd Heideman <61336585+theideman134@users.noreply.github.com>
2020-12-08 14:18:31 -0600

    Created unit tests based on the requirements

--- LeetCodeMaxNumberOfBalloons/Solution.cs
+++ LeetCodeMaxNumberOfBalloons/Solution.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace LeetCodeMaxNumberOfBalloons
 {
-    class Solution
+    public class Solution
     {
+        /*
+         https://leetcode.com/problems/maximum-number-of-balloons/
+        */
+        public int MaxNumberOfBalloons(string text)
+        {
+            return 0;
+        }
     }
 }
--- MaxNumBalloonsTest/SolutionTest.cs
+++ MaxNumBalloonsTest/SolutionTest.cs
@@ -1,3 +1,4 @@
+using LeetCodeMaxNumberOfBalloons;
 using NUnit.Framework;
 
 namespace MaxNumBalloonsTest
@@ -8,11 +9,33 @@ namespace MaxNumBalloonsTest
         public void Setup()
         {
         }
-
+        //nlaebolko output 1
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            string balloonString = "nlaebolko";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(1, result);
+        }
+
+        //loonbalxballpoon   output 2
+        [Test]
+        public void Test2()
+        {
+            string balloonString = "loonbalxballpoon";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(2, result);
+        }
+        //leetcode  output 0
+        [Test]
+        public void Test3()
+        {
+            string balloonString = "leetcode";
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(balloonString);
+            Assert.AreEqual(0, result);
         }
     }
 }
